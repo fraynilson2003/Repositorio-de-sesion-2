707d11e4842d2f298a6dbb59ce0e1a12050585f1
fraynilson2003 <>
2021-04-26 16:47:46 +0000

    cambios en repositorio languages

---
+++ AreaTriangulo.rb
@@ -0,0 +1,14 @@
+#declarar 
+print "Hola profe!\n"
+puts "Hola mundo"
+#datos de entrada
+print "Ejercicio 01\n"
+puts "Ingrese Base:"
+b=gets.to_i
+puts "Ingrese Altura:"
+h=gets.to_i
+#proceso
+area=(b*h)/2
+#datos de salida
+puts "El Area del triangulos es: ", area
+puts "chido profe"
